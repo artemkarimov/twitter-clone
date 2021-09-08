8dbfef5a852d447d93cdd173f9d911b8b29c69b6
artemkarimov <tymwrk1@gmail.com>
2021-09-08 23:51:07 +0300

    added landing page

---
+++ frontend/src/pages/Landing/Landing.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import logo from '../../assets/logo.svg';
+
+const Landing: FC = () => {
+  return <div>
+    <img src={logo} alt="twitter-logo" />
+  </div>;
+};
+
+export default Landing;
