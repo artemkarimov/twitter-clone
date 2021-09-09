42836cfb5d9cffb31a184c719fd7077c0234ffe2
artemkarimov <tymwrk1@gmail.com>
2021-09-10 02:38:48 +0300

    added side banner to landing page

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -5,13 +5,30 @@ import Button from '../../components/UI/Button/Button';
 import classes from './Landing.module.scss';
 
 const Landing: FC = () => {
-  return <div className={classes.landing}>
-    <img src={logo} alt="twitter-logo" />
-    <h1>See what's happening in <br /> the world right now</h1>
-    <h2>Join Twitter today.</h2>
-    <Button width="23rem">Sign up</Button>
-    <Button flat={true} width="23rem">Log in</Button>
-  </div>;
+  return (
+    <div className={classes.landing}>
+      <div className={classes['side-banner']}>
+        <img src={logo} alt="twitter-logo" />
+        <ul>
+          <li>Follow your interests.</li>
+          <li>Share your thoughts.</li>
+          <li>Hear what people are talking about.</li>
+          <li>Join the conversations.</li>
+        </ul>
+      </div>
+      <div className={classes.content}>
+        <img src={logo} alt="twitter-logo" />
+        <h1>
+          See what's happening in <br /> the world right now
+        </h1>
+        <h2>Join Twitter today.</h2>
+        <Button width="23rem">Sign up</Button>
+        <Button flat={true} width="23rem">
+          Log in
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
