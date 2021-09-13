b49a3f3fcaa292ce534e5d4f12df573b3e3251d8
artemkarimov <tymwrk1@gmail.com>
2021-09-13 21:54:00 +0300

    configured router and added paths to pages

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,11 +1,26 @@
 import { FC } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Landing from './Landing/Landing';
+import Login from './Login/Login';
+import Register from './Register/Register';
 
 const App: FC = () => {
-  return <div>
-    <Landing />
-  </div>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact>
+          <Landing />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </>
+  );
 };
 
 export default App;
