85cdd2cea0cf17d0fd8f1303b01da5114345e82f
artemkarimov <tymwrk1@gmail.com>
2021-09-20 19:14:53 +0300

    added structure and styling to select component

--- frontend/src/components/UI/Select/index.tsx
+++ frontend/src/components/UI/Select/index.tsx
@@ -1,7 +1,25 @@
 import { FC } from 'react';
 
+import styles from './styles.module.scss';
+
 const Select: FC = () => {
-  return <></>;
+  const years = [];
+  const currentYear = new Date().getFullYear();
+  for (let i = currentYear; i >= 1901; i--) {
+    years.push(i);
+  }
+  return (
+    <div className={styles['select-box']}>
+      <select name="Year">
+        <option disabled selected></option>
+        {years.map(year => (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
 
 export default Select;
