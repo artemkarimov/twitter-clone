f63b506b9311e24f0315198aad602dbfce5ce4b0
artemkarimov <tymwrk1@gmail.com>
2021-09-30 23:58:28 +0300

    added submit handler to login page

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from 'react';
+import { FC, useRef, FormEvent } from 'react';
 
 import Input from 'components/UI/Input';
 import Button from 'components/UI/Button';
@@ -8,13 +8,21 @@ import styles from './styles.module.scss';
 const Login: FC = () => {
   const loginRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const login = loginRef.current?.value;
+    const password = passwordRef.current?.value;
+    console.log(login, password);
+  };
   return (
     <div className={styles.login}>
       <img src={logo} alt="logo" />
       <h1>Log in to Twitter</h1>
-      <Input type="text" placeholder="Phone, email or username" reference={loginRef} />
-      <Input type="password" placeholder="Password" reference={passwordRef} />
-      <Button width="20.4rem">Log In</Button>
+      <form onSubmit={submitHandler}>
+        <Input type="text" placeholder="Phone, email or username" reference={loginRef} />
+        <Input type="password" placeholder="Password" reference={passwordRef} />
+        <Button width="20.4rem">Log In</Button>
+      </form>
     </div>
   );
 };
