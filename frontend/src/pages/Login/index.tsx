544b72ad4378aa9687de5649cfc91a4a5f67aced
artemkarimov <tymwrk1@gmail.com>
2021-10-04 23:56:56 +0300

    changed login page logic and modified card component styling

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -2,12 +2,14 @@ import { FC, useState, ChangeEvent, FormEvent } from 'react';
 
 import Input from 'components/UI/Input';
 import Button from 'components/UI/Button';
+import Card from 'components/UI/Card';
 import logo from 'assets/logo.svg';
 import styles from './styles.module.scss';
 
 const Login: FC = () => {
   const [login, setLogin] = useState<string>();
   const [password, setPassword] = useState<string>();
+  const [loginIsEntered, setLoginIsEntered] = useState<boolean>(false);
 
   const loginChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setLogin(event.target.value);
@@ -20,22 +22,27 @@ const Login: FC = () => {
   const submitHandler = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
-  
+
   return (
-    <div className={styles.login}>
-      <img src={logo} alt="logo" />
-      <h1>Log in to Twitter</h1>
-      <form onSubmit={submitHandler}>
-        <Input
-          type="text"
-          label="Phone, email or username"
-          // placeholder="Phone, email or username"
-          changeHandler={loginChangeHandler}
-        />
-        <Input type="password" label="Password" changeHandler={passwordChangeHandler} />
-        <Button width="20.4rem">Log In</Button>
-      </form>
-    </div>
+    <Card>
+      <div className={styles.login}>
+        <img src={logo} alt="logo" className={styles.logo} />
+        <h1>To get started, first enter your phone, email, or @username</h1>
+        <form onSubmit={submitHandler}>
+          {!loginIsEntered && (
+            <Input
+              type="text"
+              label="Phone, email or username"
+              changeHandler={loginChangeHandler}
+            />
+          )}
+          {loginIsEntered && (
+            <Input type="password" label="Password" changeHandler={passwordChangeHandler} />
+          )}
+          <Button width="32.4rem">Log In</Button>
+        </form>
+      </div>
+    </Card>
   );
 };
 
