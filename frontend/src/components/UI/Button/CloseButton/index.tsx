1094c3967f16df794ce823f31c58a87d8040c8bb
artemkarimov <tymwrk1@gmail.com>
2021-10-10 22:21:04 +0300

    added icon to close button

--- frontend/src/components/UI/Button/CloseButton/index.tsx
+++ frontend/src/components/UI/Button/CloseButton/index.tsx
@@ -3,7 +3,12 @@ import { FC } from 'react';
 import styles from './styles.module.scss';
 
 const CloseButton: FC = () => {
-  return <button className={styles['close_button']}></button>;
+  return (
+    <button className={styles['close_button']}>
+      <div></div>
+      <div></div>
+    </button>
+  );
 };
 
 export default CloseButton;
