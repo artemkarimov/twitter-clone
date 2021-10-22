05176e3cda0d4de263ad8e8cdc1a09308646bd35
artemkarimov <tymwrk1@gmail.com>
2021-10-22 23:59:25 +0300

    added input component and styles to home page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
 
+import Input from 'components/UI/Input';
+import styles from './styles.module.scss';
+
 const Home: FC = () => {
   return (
-    <>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h2>Home</h2>
+      <Input type="text" label="What's happening?"/>
       <h1>Welcome to Twitter</h1>
-    </>
+    </div>
   );
 };
 
